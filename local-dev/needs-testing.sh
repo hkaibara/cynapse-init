14bff098f6c30ab5e35a7d928a0ca4eebe9985ed
hkaibara <hiroshi.kaibara.hk@gmail.com>
2026-02-15 12:35:46 +0800

    init

--- local-dev/needs-testing.sh
+++ local-dev/needs-testing.sh
@@ -17,10 +17,11 @@ sudo apt install -y openjdk-17-jdk docker.io nginx git curl gnupg openssl
 echo "=== Adding Jenkins GPG key and Repository ==="
 sudo mkdir -p /usr/share/keyrings
 # Download and de-armor the key so it's in a format apt always understands
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | 
-  sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
-/etc/apt/sources.list.d/jenkins.list'
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt update
 sudo apt install -y jenkins
